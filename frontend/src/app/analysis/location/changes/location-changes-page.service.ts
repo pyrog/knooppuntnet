0f8b69b03ffc83f140c5c3d38138d459c9e8f533
vmarc <marc@vmarc.be>
2024-05-12 09:54:19 +0200

    #415 location changes page query parameters

--- frontend/src/app/analysis/location/changes/location-changes-page.service.ts
+++ frontend/src/app/analysis/location/changes/location-changes-page.service.ts
@@ -1,12 +1,18 @@
 import { computed } from '@angular/core';
 import { signal } from '@angular/core';
 import { inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { Params } from '@angular/router';
 import { ChangesParameters } from '@api/common/changes/filter';
 import { LocationChangesPage } from '@api/common/location';
 import { ApiResponse } from '@api/custom';
+import { Util } from '@app/components/shared';
+import { AnalysisStrategy } from '@app/core';
 import { PreferencesService } from '@app/core';
 import { ChangeOption } from '@app/kpn/common';
 import { ApiService } from '@app/services';
+import { PageParams } from '@app/shared/base';
 import { RouterService } from '../../../shared/services/router.service';
 import { UserService } from '../../../shared/user';
 import { LocationService } from '../location.service';
@@ -17,6 +23,8 @@ export class LocationChangesPageService {
   private readonly routerService = inject(RouterService);
   private readonly preferencesService = inject(PreferencesService);
   private readonly userService = inject(UserService);
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
 
   private readonly _response = signal<ApiResponse<LocationChangesPage> | null>(null);
   private readonly _changesParameters = signal<ChangesParameters>(null);
@@ -32,15 +40,14 @@ export class LocationChangesPageService {
 
   onInit() {
     this.locationService.initPage(this.routerService);
-    const parameters: ChangesParameters = {
-      year: null,
-      month: null,
-      day: null,
-      pageSize: 25,
-      pageIndex: 0,
-      impact: false,
-    };
-    this._changesParameters.set(parameters);
+    const uniqueQueryParams = Util.uniqueParams(this.routerService.queryParams());
+    const pageParams = new PageParams(this.routerService.params(), uniqueQueryParams);
+    const strategy = pageParams.strategy(this.preferencesService.strategy());
+    const changesParameters = pageParams.changesParameters(
+      this.preferencesService.impact(),
+      this.preferencesService.pageSize()
+    );
+    this._changesParameters.set(changesParameters);
     this.load();
   }
 
@@ -85,13 +92,30 @@ export class LocationChangesPageService {
   }
 
   private load() {
-    this.apiService
-      .locationChanges(this.locationService.key(), this.changesParameters())
-      .subscribe((response) => {
-        if (response.result) {
-          this.locationService.setSummary(response.result.summary);
-        }
-        this._response.set(response);
-      });
+    const promise = this.navigate(this.preferencesService.strategy(), this.changesParameters());
+    promise.then(() => {
+      this.apiService
+        .locationChanges(this.locationService.key(), this.changesParameters())
+        .subscribe((response) => {
+          if (response.result) {
+            this.locationService.setSummary(response.result.summary);
+          }
+          this._response.set(response);
+        });
+    });
+  }
+
+  private navigate(
+    strategy: AnalysisStrategy,
+    changesParameters: ChangesParameters
+  ): Promise<boolean> {
+    const queryParams: Params = {
+      strategy,
+      ...changesParameters,
+    };
+    return this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams,
+    });
   }
 }
--- frontend/src/app/shared/services/version.service.ts
+++ frontend/src/app/shared/services/version.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VersionService {
-  version = '4.3.28';
+  version = '4.3.29';
   experimental = false;
 }
